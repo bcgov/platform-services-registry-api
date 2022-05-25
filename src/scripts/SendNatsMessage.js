048f5d3c61f487ce846bf8af4b71359372c51408
Oamar Kanji <oamarkanji@gmail.com>
2022-05-25 00:35:36 -0700

    added nats env variable

--- src/scripts/SendNatsMessage.js
+++ src/scripts/SendNatsMessage.js
@@ -1,6 +1,8 @@
 const { connect, StringCodec, JSONCodec } = require("nats");
 
-const host = `nats://localhost:4222`;
+//const host = `nats://localhost:4222`;
+const host = process.env.NATS_HOST
+const port = process.env.NATS_PORT
 
 async function sendNatsMessage() {
   const nc = await connect({ servers: host });
