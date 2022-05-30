7ec01c96dddc93f15b53df5bbd466b00fc46e2fc
Oamar Kanji <oamarkanji@gmail.com>
2022-05-30 11:29:03 -0700

    uncommented nats

--- src/graphql/resolvers/Mutation/MakePrivateCloudRequestDecision.js
+++ src/graphql/resolvers/Mutation/MakePrivateCloudRequestDecision.js
@@ -98,7 +98,7 @@ async function makePrivateCloudRequestDecision(
       // subject: `${profile.name} OCP 4 Project Set`,
     });
 
-    //await sendNatsMessage();
+    await sendNatsMessage();
 
     return RequestStatus.APPROVED;
   }
