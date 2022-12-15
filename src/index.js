f8ef6069ccf1fddbc9bce435b35c1932e0df829e
okanji <oamarkanji@gmail.com>
2022-12-14 22:32:40 -0800

    enabled apollo sandbox for production

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { ApolloServer } from "apollo-server-express";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault
+} from "apollo-server-core";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { configureKeycloak } from "./auth/config";
 import { KeycloakContext, KeycloakTypeDefs } from "keycloak-connect-graphql";
@@ -17,20 +20,20 @@ import chesService from "./ches";
 import provisionerCallbackHandler from "./controllers/provisionerCallbackHandler";
 
 async function startApolloServer(typeDefs, resolvers) {
-
   let schema = makeExecutableSchema({
     typeDefs: [KeycloakTypeDefs, typeDefs],
     resolvers,
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ footer: false })]
   });
 
   schema = applyAuthDirectiveTranformers(applyDirectiveTransformers(schema));
 
   const app = express();
-  
+
   const { keycloak } = configureKeycloak(app, "/graphql");
   const httpServer = http.createServer(app);
 
-  const dataSources = await getDatasources()
+  const dataSources = await getDatasources();
 
   const server = new ApolloServer({
     schema,
@@ -38,9 +41,9 @@ async function startApolloServer(typeDefs, resolvers) {
     dataSources: () => dataSources,
     context: ({ req }) => ({
       kauth: new KeycloakContext({ req }, keycloak),
-      chesService,
+      chesService
     }),
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
   });
 
   await server.start();
@@ -48,7 +51,7 @@ async function startApolloServer(typeDefs, resolvers) {
   app.use(express.json());
   app.use(
     express.urlencoded({
-      extended: true,
+      extended: true
     })
   );
   app.post("/namespace", provisionerCallbackHandler);
