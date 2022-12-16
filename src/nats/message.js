4e4757f33f8d1def7d824983c99bed37e55048c2
okanji <oamarkanji@gmail.com>
2022-12-16 01:32:16 -0800

    hardcoded cluster

--- src/nats/message.js
+++ src/nats/message.js
@@ -20,7 +20,7 @@ function message(
     toolsQuota
   } = requestedProject;
 
-  console.log("Requested Project")
+  console.log("Requested Project");
   console.log(requestedProject);
 
   const projectOwnerContact = {
@@ -39,20 +39,22 @@ function message(
     role: "lead"
   };
 
-  const secondaryTechnicalLeadContact = secondaryTechnicalLead ? {
-    user_id: secondaryTechnicalLead.githubId,
-    provider: "github",
-    email: secondaryTechnicalLead.email,
-    rocketchat_username: null,
-    role: "lead"
-  }: null;
+  const secondaryTechnicalLeadContact = secondaryTechnicalLead
+    ? {
+        user_id: secondaryTechnicalLead.githubId,
+        provider: "github",
+        email: secondaryTechnicalLead.email,
+        rocketchat_username: null,
+        role: "lead"
+      }
+    : null;
 
   const namespaces = [
     { quotaName: "tools", quota: toolsQuota },
     { quotaName: "prod", quota: productionQuota },
     { quotaName: "dev", quota: developmentQuota },
     { quotaName: "test", quota: testQuota }
-  ].map(({quotaName, quota}) => ({
+  ].map(({ quotaName, quota }) => ({
     // namespace_id: 21,
     name: `${licencePlate}-${[quotaName]}`,
     quota: {
@@ -81,8 +83,8 @@ function message(
   const request = {
     action,
     profile_id: _id,
-    cluster_id: 3, // Cluster id e.g gold, silver, klab (enum)
-    cluster_name: cluster,
+    cluster_id: cluster,
+    cluster_name: "silver", // Cluster id e.g gold, silver, klab (enum)
     display_name: name,
     description: description,
     ministry_id: ministry,
