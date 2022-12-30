55b4056a24a0070f0ea8383c316839049053505f
okanji <oamarkanji@gmail.com>
2022-12-30 00:15:17 -0800

    tweaked relations

--- src/resolvers/mutations/requests.ts
+++ src/resolvers/mutations/requests.ts
@@ -12,6 +12,7 @@ export const privateCloudProjectRequest: MutationResolvers = async (
   { kauth, prisma }
 ) => {
   const { email: authEmail, resource_access } = kauth.accessToken.content;
+  console.log(args);
   const {
     metaData,
     commonComponents,
@@ -24,15 +25,12 @@ export const privateCloudProjectRequest: MutationResolvers = async (
     data: {
       type: RequestType.CREATE,
       status: RequestStatus.PENDING_DECISION,
-      commonComponents,
       projectOwner: {
         connectOrCreate: {
           where: {
             email: metaData.projectOwner
           },
-          create: {
-            ...projectOwner
-          }
+          create: projectOwner
         }
       },
       primaryTechnicalLead: {
@@ -40,9 +38,7 @@ export const privateCloudProjectRequest: MutationResolvers = async (
           where: {
             email: metaData.primaryTechnicalLead
           },
-          create: {
-            ...primaryTechnicalLead
-          }
+          create: primaryTechnicalLead
         }
       },
       secondaryTechnicalLead: {
@@ -50,26 +46,100 @@ export const privateCloudProjectRequest: MutationResolvers = async (
           where: {
             email: metaData.secondaryTechnicalLead
           },
-          create: {
-            ...secondaryTechnicalLead
-          }
-        }
-      },
-      createdBy: {
-        connect: {
-          email: authEmail
-        }
-      },
-      requestedProject: {
-        create: {
-          ...metaData,
-          commonComponents,
-          licensePlate: "ABC123",
-          createdBy: authEmail
+          create: secondaryTechnicalLead
         }
       }
+      // createdBy: {
+      //   connect: {
+      //     email: authEmail
+      //   }
+      // }
+      // requestedProject: {
+      //   ...metaData,
+      //   commonComponents,
+      //   licensePlate: "ABC123",
+      //   createdBy: authEmail
+      // }
     }
   });
 
   return createRequest;
 };
+
+// import {
+//   ProjectMetaDataInput,
+//   CommonComponentsInput,
+//   MutationResolvers,
+//   MutationPrivateCloudProjectRequestArgs
+// } from "__generated__/resolvers-types";
+// import { RequestType, RequestStatus, ProjectStatus } from "../enum.js";
+
+// export const privateCloudProjectRequest: MutationResolvers = async (
+//   _,
+//   args: MutationPrivateCloudProjectRequestArgs,
+//   { kauth, prisma }
+// ) => {
+//   const { email: authEmail, resource_access } = kauth.accessToken.content;
+//   console.log(args)
+//   const {
+//     metaData,
+//     commonComponents,
+//     projectOwner,
+//     primaryTechnicalLead,
+//     secondaryTechnicalLead
+//   } = args;
+
+//   const createRequest = await prisma.privateCloudRequest.create({
+//     data: {
+//       type: RequestType.CREATE,
+//       status: RequestStatus.PENDING_DECISION,
+//       projectOwner: {
+//         connectOrCreate: {
+//           where: {
+//             email: metaData.projectOwner
+//           },
+//           create: {
+//             email: projectOwner.email,
+//             firstName: projectOwner.firstName,
+//             lastName: projectOwner.lastName,
+//             githubId: projectOwner.githubId,
+//             ministry: projectOwner.ministry
+
+//             // ...projectOwner
+//           }
+//         }
+//       },
+//       primaryTechnicalLead: {
+//         connectOrCreate: {
+//           where: {
+//             email: metaData.primaryTechnicalLead
+//           },
+//           create: primaryTechnicalLead
+//         }
+//       },
+//       secondaryTechnicalLead: {
+//         connectOrCreate: {
+//           where: {
+//             email: metaData.secondaryTechnicalLead
+//           },
+//           create: {
+//             ...secondaryTechnicalLead
+//           }
+//         }
+//       },
+//       createdBy: {
+//         connect: {
+//           email: authEmail
+//         }
+//       },
+//       requestedProject: {
+//         ...metaData,
+//         commonComponents,
+//         licensePlate: "ABC123",
+//         createdBy: authEmail
+//       }
+//     }
+//   });
+
+//   return createRequest;
+// };
