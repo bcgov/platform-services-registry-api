c6663e3a5e7d676711113bc93c888af28fe69045
okanji <oamarkanji@gmail.com>
2022-12-31 16:14:30 -0800

    Added new error if decsion is already made

--- src/resolvers/mutations/makePrivateCloudRequestCreateDecision.ts
+++ src/resolvers/mutations/makePrivateCloudRequestCreateDecision.ts
@@ -2,7 +2,8 @@ import {
   MutationMakePrivateCloudRequestCreateDecisionArgs,
   MutationResolvers
 } from "__generated__/resolvers-types";
-import { RequestDecision } from "../enum.js";
+import { RequestDecision, RequestDecisionStatus } from "../enum.js";
+import { Prisma as PrismaType } from "@prisma/client";
 
 const makePrivateCloudRequestCreateDecision: MutationResolvers = async (
   _,
@@ -15,17 +16,29 @@ const makePrivateCloudRequestCreateDecision: MutationResolvers = async (
     throw new Error("You must be an admin to make a request decision.");
   }
 
-  const request = await prisma.privateCloudRequest.update({
-    where: {
-      id: requestId
-    },
-    data: {
-      status: decision,
-      active: decision === RequestDecision.APPROVED,
-      decisionDate: new Date(),
-      decisionMakerEmail: authEmail
+  let request;
+
+  try {
+    request = await prisma.privateCloudRequest.update({
+      where: {
+        id: requestId,
+        status: RequestDecisionStatus.PENDING
+      },
+      data: {
+        status: decision,
+        active: decision === RequestDecision.APPROVED,
+        decisionDate: new Date(),
+        decisionMakerEmail: authEmail
+      }
+    });
+  } catch (e) {
+    if (e instanceof PrismaType.PrismaClientKnownRequestError) {
+      if (e.code === "P2025") {
+        throw new Error("Request not found or already has a decision.");
+      }
     }
-  });
+    throw e;
+  }
 
   return request;
 };
