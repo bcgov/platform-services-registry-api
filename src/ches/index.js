a3148b3fc7101d27995b9f73f22c3bd2d0fdbd2d
unknown <kolezhannochka@gmail.com>
2023-01-26 11:03:25 -0800

    main merge

--- src/ches/index.js
+++ src/ches/index.js
@@ -2,11 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import ChesService from "./chesService.js";
 
-const newChesService = new ChesService({
+export default new ChesService({
   tokenUrl: process.env.CHES_TOKEN_URL,
   clientId: process.env.CHES_CLIENT_ID,
   clientSecret: process.env.CHES_CLIENT_SECRET,
   apiUrl: process.env.CHES_API_URL,
 });
-
-export default newChesService
