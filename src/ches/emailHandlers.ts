7d7468a6b3b60ffb453efb0b5a088b31db702b3c
Zhanna <kolezhannochka@gmail.com>
2023-03-02 14:44:35 -0800

    ministries list updated, converted to human friendly names for emails

--- src/ches/emailHandlers.ts
+++ src/ches/emailHandlers.ts
@@ -11,46 +11,9 @@ import {
   DefaultMemoryOptions,
   DefaultStorageOptions,
   adminEmails,
+  clusterNames,
+  ministriesNames,
 } from "../nats/constants.js";
-
-const adminEmails = ['zhanna.kolesnyk@gov.bc.ca'];
-const clusterNames = [
-  {
-    id: 1,
-    name: "clab",
-    humanFriendlyName: "CLAB Calgary",
-  },
-  {
-    id: 2,
-    name: "klab",
-    humanFriendlyName: "KLAB Kamloops",
-  },
-  {
-    id: 3,
-    name: "silver",
-    humanFriendlyName: "Silver Kamloops",
-  },
-  {
-    id: 4,
-    name: "gold",
-    humanFriendlyName: "Gold Kamloops",
-  },
-  {
-    id: 5,
-    name: "golddr",
-    humanFriendlyName: "Gold (DR) Calgary",
-  },
-  {
-    id: 6,
-    name: "klab2",
-    humanFriendlyName: "KLAB2 Kamloops",
-  },
-  {
-    id: 7,
-    name: "emerald",
-    humanFriendlyName: "Emerald Hosting Tier",
-  },
-];
 
 export const isQuotaChanged = (projectQuota, requestedQuota) =>
   JSON.stringify(projectQuota) !== JSON.stringify(requestedQuota);
@@ -227,7 +190,10 @@ export const generateEmailTemplateData = (
     licencePlate: requestedProject.licencePlate,
     projectName: requestedProject.name,
     productDescription: requestedProject.description,
-    productMinistry: requestedProject.ministry,
+    productMinistry: ministriesNames.filter(
+      (item) =>
+        item.name.toLowerCase() === requestedProject?.ministry.toLowerCase()
+    )[0]?.humanFriendlyName,
     POName: `${projectOwner.firstName} ${projectOwner.lastName}`,
     POEmail: projectOwner.email,
     POGitHubOrIDIR: projectOwner?.POIDIR || projectOwner?.githubId,
