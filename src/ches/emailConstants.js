86dc66d511054defe0136f6eb915414f90c19e42
unknown <kolezhannochka@gmail.com>
2023-03-06 16:48:19 -0800

    updated ministies list not in use, old one is

--- src/ches/emailConstants.js
+++ src/ches/emailConstants.js
@@ -1,207 +1,306 @@
 export const adminEmails = [];
 
+export const ministriesNames = [
+  {
+    id: 1,
+    name: "AEST",
+    humanFriendlyName: "Post-Secondary Education and Future Skills Contacts"
+  },
+  {
+    id: 2,
+    name: "AG",
+    humanFriendlyName: "Attorney General"
+  },
+  {
+    id: 3,
+    name: "AGRI",
+    humanFriendlyName: "Agriculture and Food"
+  },
+  {
+    id: 4,
+    name: "ALC",
+    humanFriendlyName: "Advisory Committee Revitalization"
+  },
+  {
+    id: 5,
+    name: "BCPC",
+    humanFriendlyName: "British Columbia Provincial Committee"
+  },
+  {
+    id: 5,
+    name: "BCPC",
+    humanFriendlyName: "British Columbia Provincial Committee"
+  },
+  {
+    id: 6,
+    name: "CITZ",
+    humanFriendlyName: "Citizens' Services"
+  },
+  {
+    id: 7,
+    name: "DBC",
+    humanFriendlyName: "Drug Benefit Council"
+  },
+  {
+    id: 8,
+    name: "EAO",
+    humanFriendlyName: "Environmental Assessment Office"
+  },
+  {
+    id: 9,
+    name: "EDUC",
+    humanFriendlyName: "Education and Child Care"
+  },
+  {
+    id: 10,
+    name: "EMBC",
+    humanFriendlyName: "Emergency management"
+  },
+  {
+    id: 11,
+    name: "EMPR",
+    humanFriendlyName: "Energy, Mines and Low Carbon Innovation"
+  },
+  {
+    id: 12,
+    name: "ENV",
+    humanFriendlyName: "Environment and Climate Change Strategy"
+  },
+  {
+    id: 13,
+    name: "FIN",
+    humanFriendlyName: "Finance"
+  },
+  {
+    id: 14,
+    name: "FIN",
+    humanFriendlyName: "Finance"
+  },
+  {
+    id: 15,
+    name: "FLNR",
+    humanFriendlyName: "Forests, Lands, Natural Resource"
+  },
+  {
+    id: 16,
+    name: "HLTH",
+    humanFriendlyName: "Health"
+  },
+  {
+    id: 17,
+    name: "IRR",
+    humanFriendlyName: "Indigenous Relations & Reconciliation"
+  },
+  {
+    id: 18,
+    name: "JEDC",
+    humanFriendlyName: "Jobs, Economic Development and Innovation"
+  },
+  {
+    id: 19,
+    name: "LBR",
+    humanFriendlyName: "Labour"
+  },
+  {
+    id: 20,
+    name: "LDB",
+    humanFriendlyName: "Liquor Distribution Branch"
+  },
+  {
+    id: 21,
+    name: "MAH",
+    humanFriendlyName: "Municipal Affairs and Housing"
+  },
+  {
+    id: 22,
+    name: "MCF",
+    humanFriendlyName: "Children and Family Development"
+  },
+  {
+    id: 23,
+    name: "MMHA",
+    humanFriendlyName: "Mental Health and Addictions"
+  },
+  {
+    id: 24,
+    name: "PSA",
+    humanFriendlyName: "Public Service Agency"
+  },
+  {
+    id: 25,
+    name: "PSSG",
+    humanFriendlyName: "Public Safety and Solicitor General"
+  },
+  {
+    id: 26,
+    name: "SDPR",
+    humanFriendlyName: "Social Development and Poverty Reduction"
+  },
+  {
+    id: 28,
+    name: "TCA",
+    humanFriendlyName: "Tangible Capital Assets"
+  },
+  {
+    id: 29,
+    name: "TRAN",
+    humanFriendlyName: "Transportation and Infrastructure"
+  },
+];
+
+const ministriesNamesUpdated = [
+  {
+    id: 1,
+    name: "AF",
+    humanFriendlyName: "Agriculture and Food"
+  },
+  {
+    id: 2,
+    name: "AG",
+    humanFriendlyName: "Attorney General"
+  },
+  {
+    id: 3,
+    name: "MSFD",
+    humanFriendlyName: "Children and Family Development"
+  },
+  {
+    id: 4,
+    name: "CITZ",
+    humanFriendlyName: "Citizens' Services"
+  },
+  {
+    id: 5,
+    name: "ECC",
+    humanFriendlyName: "Education and Child Care"
+  },
+  {
+    id: 6,
+    name: "EMCR",
+    humanFriendlyName: "Emergency Management and Climate Readiness"
+  },
+  {
+    id: 7,
+    name: "EMLI",
+    humanFriendlyName: "Energy, Mines and Low Carbon Innovation"
+  },
+  {
+    id: 8,
+    name: "ENV",
+    humanFriendlyName: "Environment and Climate Change Strategy"
+  },
+  {
+    id: 9,
+    name: "FIN",
+    humanFriendlyName: "Finance"
+  },
+  {
+    id: 10,
+    name: "FOR",
+    humanFriendlyName: "Forests"
+  },
+  {
+    id: 11,
+    name: "HLTH",
+    humanFriendlyName: "Health"
+  },
+  {
+    id: 12,
+    name: "HOUS",
+    humanFriendlyName: "Housing"
+  },
+  {
+    id: 13,
+    name: "IRR",
+    humanFriendlyName: "Indigenous Relations & Reconciliation"
+  },
+  {
+    id: 14,
+    name: "JEDI",
+    humanFriendlyName: "Jobs, Economic Development and Innovation"
+  },
+  {
+    id: 15,
+    name: "LBR",
+    humanFriendlyName: "Labour"
+  },
+  {
+    id: 16,
+    name: "MMHA",
+    humanFriendlyName: "Mental Health and Addictions"
+  },
+  {
+    id: 17,
+    name: "MUNI",
+    humanFriendlyName: "Municipal Affairs"
+  },
+  {
+    id: 18,
+    name: "PSFS",
+    humanFriendlyName: "Post-Secondary Education and Future Skills"
+  },
+  {
+    id: 19,
+    name: "PSSG",
+    humanFriendlyName: "Public Safety and Solicitor General"
+  },
+  {
+    id: 20,
+    name: "SDPR",
+    humanFriendlyName: "Social Development and Poverty Reduction"
+  },
+  {
+    id: 21,
+    name: "TACS",
+    humanFriendlyName: "Tourism, Arts, Culture and Sport"
+  },
+  {
+    id: 22,
+    name: "MOTI",
+    humanFriendlyName: "Transportation and Infrastructure"
+  },
+  {
+    id: 23,
+    name: "WLRS",
+    humanFriendlyName: "Water, Land and Resource Stewardship"
+  },
+];
+
+
 export const clusterNames = [
   {
     id: 1,
     name: "clab",
-    humanFriendlyName: "CLAB Calgary"
+    humanFriendlyName: "CLAB Calgary",
   },
   {
     id: 2,
     name: "klab",
-    humanFriendlyName: "KLAB Kamloops"
+    humanFriendlyName: "KLAB Kamloops",
   },
   {
     id: 3,
     name: "silver",
-    humanFriendlyName: "Silver Kamloops"
+    humanFriendlyName: "Silver Kamloops",
   },
   {
     id: 4,
     name: "gold",
-    humanFriendlyName: "Gold Kamloops"
+    humanFriendlyName: "Gold Kamloops",
   },
   {
     id: 5,
     name: "golddr",
-    humanFriendlyName: "Gold (DR) Calgary"
+    humanFriendlyName: "Gold (DR) Calgary",
   },
   {
     id: 6,
     name: "klab2",
-    humanFriendlyName: "KLAB2 Kamloops"
+    humanFriendlyName: "KLAB2 Kamloops",
   },
   {
     id: 7,
     name: "emerald",
-    humanFriendlyName: "Emerald Hosting Tier"
-  }
+    humanFriendlyName: "Emerald Hosting Tier",
+  },
 ];
-
-export const emailData = (
-  requestedProject,
-  project,
-  projectOwner,
-  primaryTechnicalLead,
-  secondaryTechnicalLead,
-  obj
-) => {
-  const objMain = {
-    consoleURLProdNameSpace: `https://console.apps.${requestedProject.cluster}.devops.gov.bc.ca/topology/ns/${requestedProject.licencePlate}-prod`,
-    isProductionQuotaChanged:
-      requestedProject &&
-      project &&
-      JSON.stringify(project.productionQuota) !==
-        JSON.stringify(requestedProject.productionQuota),
-    productionQuotaCPURequested:
-      requestedProject &&
-      project &&
-      requestedProject.productionQuota.cpuRequests !==
-        project.productionQuota.cpuRequests
-        ? requestedProject.productionQuota.cpuRequests
-        : null,
-    productionQuotaMemoryRequested:
-      requestedProject &&
-      project &&
-      requestedProject.productionQuota.memoryRequests !==
-        project.productionQuota.memoryRequests
-        ? requestedProject.productionQuota.memoryRequests
-        : null,
-    productionQuotaStorageRequested:
-      requestedProject &&
-      project &&
-      requestedProject.productionQuota.storageFile !==
-        project.productionQuota.storageFile
-        ? requestedProject.productionQuota.storageFile
-        : null,
-    isDevelopmentQuotaChanged:
-      requestedProject &&
-      project &&
-      JSON.stringify(project.developmentQuota) !==
-        JSON.stringify(requestedProject.developmentQuota),
-    consoleURLDevNameSpace: `https://console.apps.${requestedProject.cluster}.devops.gov.bc.ca/topology/ns/${requestedProject.licencePlate}-dev`,
-    developmentQuotaCPURequested:
-      requestedProject &&
-      project &&
-      requestedProject.developmentQuota.cpuRequests !==
-        project.developmentQuota.cpuRequests
-        ? requestedProject.developmentQuota.cpuRequests
-        : null,
-    developmentQuotaMemoryRequested:
-      requestedProject &&
-      project &&
-      requestedProject.developmentQuota.memoryRequests !==
-        project.developmentQuota.memoryRequests
-        ? requestedProject.developmentQuota.memoryRequests
-        : null,
-    developmentQuotaStorageRequested:
-      requestedProject &&
-      project &&
-      requestedProject.developmentQuota.storageFile !==
-        project.developmentQuota.storageFile
-        ? requestedProject.developmentQuota.storageFile
-        : null,
-    isTestQuotaChanged:
-      requestedProject &&
-      project &&
-      JSON.stringify(project.testQuota) !==
-        JSON.stringify(requestedProject.testQuota),
-    consoleURLTestNameSpace: `https://console.apps.${requestedProject.cluster}.devops.gov.bc.ca/topology/ns/${requestedProject.licencePlate}-test`,
-    testQuotaCPURequested:
-      requestedProject &&
-      project &&
-      requestedProject.testQuota.cpuRequests !== project.testQuota.cpuRequests
-        ? requestedProject.testQuota.cpuRequests
-        : null,
-    testQuotaMemoryRequested:
-      requestedProject &&
-      project &&
-      requestedProject.testQuota.memoryRequests !==
-        project.testQuota.memoryRequests
-        ? requestedProject.testQuota.memoryRequests
-        : null,
-    testQuotaStorageRequested:
-      requestedProject &&
-      project &&
-      requestedProject.testQuota.storageFile !== project.testQuota.storageFile
-        ? requestedProject.testQuota.storageFile
-        : null,
-    isToolsQuotaChanged:
-      requestedProject &&
-      project &&
-      JSON.stringify(project.toolsQuota) !==
-        JSON.stringify(requestedProject.toolsQuota),
-    consoleURLToolNameSpace: `https://console.apps.${requestedProject.cluster}.devops.gov.bc.ca/topology/ns/${requestedProject.licencePlate}-tool`,
-    toolsQuotaCPURequested:
-      requestedProject &&
-      project &&
-      requestedProject.toolsQuota.cpuRequests !== project.toolsQuota.cpuRequests
-        ? requestedProject.toolsQuota.cpuRequests
-        : null,
-    toolsQuotaMemoryRequested:
-      requestedProject &&
-      project &&
-      requestedProject.toolsQuota.memoryRequests !==
-        project.toolsQuota.memoryRequests
-        ? requestedProject.toolsQuota.memoryRequests
-        : null,
-    toolsQuotaStorageRequested:
-      requestedProject &&
-      project &&
-      requestedProject.toolsQuota.storageFile !== project.toolsQuota.storageFile
-        ? requestedProject.toolsQuota.storageFile
-        : null,
-    productionQuotaCPUCurrent: project
-      ? project.productionQuota.cpuRequests
-      : null,
-    productionQuotaMemoryCurrent: project
-      ? project.productionQuota.memoryRequests
-      : null,
-    productionQuotaStorageCurrent: project
-      ? project.productionQuota.storageFile
-      : null,
-    developmentQuotaCPUCurrent: project
-      ? project.developmentQuota.cpuRequests
-      : null,
-    developmentQuotaMemoryCurrent: project
-      ? project.developmentQuota.memoryRequests
-      : null,
-    developmentQuotaStorageCurrent: project
-      ? project.developmentQuota.storageFile
-      : null,
-    testQuotaCPUCurrent: project ? project.testQuota.cpuRequests : null,
-    testQuotaMemoryCurrent: project ? project.testQuota.memoryRequests : null,
-    testQuotaStorageCurrent: project ? project.testQuota.storageFile : null,
-    toolsQuotaCPUCurrent: project ? project.toolsQuota.cpuRequests : null,
-    toolsQuotaMemoryCurrent: project ? project.toolsQuota.memoryRequests : null,
-    toolsQuotaStorageCurrent: project ? project.toolsQuota.storageFile : null,
-    licencePlate: requestedProject.licencePlate,
-    projectName: requestedProject.name,
-    POName: `${projectOwner.firstName} ${projectOwner.lastName}`,
-    POEmail: projectOwner.email,
-    POGitHubOrIDIR: projectOwner.POIDIR
-      ? projectOwner.POIDIR
-      : projectOwner.githubId,
-    PriTLName: `${primaryTechnicalLead.firstName} ${primaryTechnicalLead.lastName}`,
-    PriTLEmail: primaryTechnicalLead.email,
-    PriTLGitHubOrIDIR: primaryTechnicalLead.POIDIR
-      ? primaryTechnicalLead.POIDIR
-      : primaryTechnicalLead.githubId,
-    SecTLName: secondaryTechnicalLead
-      ? `${secondaryTechnicalLead.firstName} ${secondaryTechnicalLead.lastName}`
-      : null,
-    SecTLEmail: secondaryTechnicalLead ? secondaryTechnicalLead.email : null,
-    SecTLGitHubOrIDIR: secondaryTechnicalLead
-      ? secondaryTechnicalLead.POIDIR
-        ? secondaryTechnicalLead.POIDIR
-        : secondaryTechnicalLead.githubId
-      : null,
-    setCluster: clusterNames.filter(
-      (item) => item.name === requestedProject.cluster
-    )[0].humanFriendlyName
-  };
-  return {
-    ...objMain,
-    ...obj
-  };
-};
