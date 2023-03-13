7054bfeecee3b5f624fd353532d551dde76247a9
Oamar <okanji@Oamars-MacBook-Pro.local>
2023-03-13 15:08:37 -0700

    changed error handling in nats private cloud test

--- src/controllers/invokeNats.ts
+++ src/controllers/invokeNats.ts
@@ -1,36 +1,43 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { connect, StringCodec, JSONCodec } from "nats";
+import { connect, JSONCodec } from "nats";
 
 const serverURL = `${process.env.NATS_HOST}:${process.env.NATS_PORT}`;
 
+const jc = JSONCodec();
+
 async function sendNatsMessage(subject, message) {
+  console.log("NATS SUBJECT: ", subject);
+  console.log("MESSAGE BODY: ", message);
+
   try {
-    console.log("NATS SUBJECT: ", subject);
-    console.log("MESSAGE BODY: ", message);
-
     const nc = await connect({ servers: serverURL });
-
-    // const sc = StringCodec();
     const jc = JSONCodec();
 
     nc.publish(subject, jc.encode(message));
-
-    await nc.drain();
-  } catch (e) {
-    console.log(`Error sending NATS message with subject: ${subject}`, e);
+    await nc.flush();
+    await nc.close();
+  } catch (err) {
+    console.error(`Error sending NATS message with subject: ${subject}`, err);
+    throw new Error("Failed to send message to NATS server");
   }
 }
 
-const invokeNats = async (req, res) => {
+const invokeNats = async (req, res, next) => {
   try {
     const { subject, message } = req.body;
+    console.log("subject: ", subject);
+
+    if (!subject || !message) {
+      throw new Error("Missing subject or message");
+    }
+
     await sendNatsMessage(subject, message);
 
     res.status(200).end();
-  } catch (e) {
-    console.log("Error invoking NATS", e);
-    res.status(500).end();
+  } catch (err) {
+    console.error("Error invoking NATS:", err);
+    next(err);
   }
 };
 
