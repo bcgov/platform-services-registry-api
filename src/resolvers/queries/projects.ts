91655be9d05b96a316a17c5dfa82106204e05555
unknown <kolezhannochka@gmail.com>
2023-04-06 14:46:02 -0700

    sort implemented

--- src/resolvers/queries/projects.ts
+++ src/resolvers/queries/projects.ts
@@ -73,13 +73,7 @@ export const privateCloudProjectsPaginated = async (_, args, { prisma }) => {
 
   const offset = page > 0 ? (page - 1) * pageSize : 0;
 
-  // const rawMngo = await prisma.privateCloudProject.findRaw({
-  //   filter: { name: {$regex: 'OpenShift 201 Test Project 1' } },
-  // })
-
-  // console.log(rawMngo)
-
-  const projectsTest = await prisma.privateCloudProject.aggregateRaw({
+  const projects = await prisma.privateCloudProject.aggregateRaw({
     pipeline: [
       {
         $project: {
@@ -89,18 +83,29 @@ export const privateCloudProjectsPaginated = async (_, args, { prisma }) => {
           "name": 1,
           "licencePlate": 1,
           "archived": 1,
-          "created": 1,
+          "created": {
+            $toString: "$created"
+          },
           "description": 1,
           "status": 1,
           "projectOwnerId": {
             $toString: "$projectOwnerId"
-          },
+          },          
           "secondaryTechnicalLeadId": {
             $toString: "$secondaryTechnicalLeadId"
           },
           "primaryTechnicalLeadId": {
             $toString: "$primaryTechnicalLeadId"
           },
+          "projectOwnerIdSearch": {
+            $toObjectId: "$projectOwnerId"
+          },  
+          "primaryTechnicalLeadIdSearch": {
+            $toObjectId: "$primaryTechnicalLeadId"
+          },  
+          "secondaryTechnicalLeadIdSearch": {
+            $toObjectId: "$secondaryTechnicalLeadId"
+          },  
           "ministry": 1,
           "cluster": 1,
           "productionQuota": 1,
@@ -116,25 +121,25 @@ export const privateCloudProjectsPaginated = async (_, args, { prisma }) => {
           }
         }
       },
-      // {
-      //   $lookup:
-      //   {
-      //     from: "User",
-      //     localField: "projectOwnerId",
-      //     foreignField: "_id",
-      //     as: "projectOwner"
-      //   }
-      // },
-      // {
-      //   $unwind: {
-      //     path: '$projectOwner',
-      //   },
-      // },
+      {
+        $lookup:
+        {
+          from: "User",
+          localField: "projectOwnerIdSearch",
+          foreignField: "_id",
+          as: "projectOwner"
+        }
+      },
+      {
+        $unwind: {
+          path: '$projectOwner',
+        },
+      },
           {
         $lookup:
         {
           from: "User",
-          localField: "secondaryTechnicalLeadId",
+          localField: "secondaryTechnicalLeadIdSearch",
           foreignField: "_id",
           as: "secondaryTechnicalLead"
         }
@@ -149,7 +154,7 @@ export const privateCloudProjectsPaginated = async (_, args, { prisma }) => {
         $lookup:
         {
           from: "User",
-          localField: "primaryTechnicalLeadId",
+          localField: "primaryTechnicalLeadIdSearch",
           foreignField: "_id",
           as: "primaryTechnicalLead"
         }
@@ -218,47 +223,47 @@ export const privateCloudProjectsPaginated = async (_, args, { prisma }) => {
       }
     ],
   })
-  // console.log(rawMongoAgg)
 
-  const projects = await prisma.privateCloudProject.findMany({
-    orderBy: {
-      name: "asc"
-    },
-    where: {
-      status: "ACTIVE",
-      AND: [
-        {
-          OR: [
-            { projectOwner: { email: { contains: search } } },
-            { projectOwner: { firstName: { contains: search } } },
-            { projectOwner: { lastName: { contains: search } } },
-            { primaryTechnicalLead: { email: { contains: search } } },
-            { primaryTechnicalLead: { firstName: { contains: search } } },
-            { primaryTechnicalLead: { lastName: { contains: search } } },
-            { secondaryTechnicalLead: { email: { contains: search } } },
-            { secondaryTechnicalLead: { firstName: { contains: search } } },
-            { secondaryTechnicalLead: { lastName: { contains: search } } },
-            { name: { contains: search } },
-            { description: { contains: search } },
-            { licencePlate: { contains: search } }
-          ]
-        },
-        {
-          ministry: {
-            in: ministry
-          }
-        },
-        {
-          cluster: {
-            in: cluster
-          }
-        }
-      ]
-    },
-    skip: offset,
-    take: pageSize
-  });
-  // console.log(projects.slice(0, 1))
+
+  // const projectsDD = await prisma.privateCloudProject.findMany({
+  //   orderBy: {
+  //     name: "asc"
+  //   },
+  //   where: {
+  //     status: "ACTIVE",
+  //     AND: [
+  //       {
+  //         OR: [
+  //           { projectOwner: { email: { contains: search } } },
+  //           { projectOwner: { firstName: { contains: search } } },
+  //           { projectOwner: { lastName: { contains: search } } },
+  //           { primaryTechnicalLead: { email: { contains: search } } },
+  //           { primaryTechnicalLead: { firstName: { contains: search } } },
+  //           { primaryTechnicalLead: { lastName: { contains: search } } },
+  //           { secondaryTechnicalLead: { email: { contains: search } } },
+  //           { secondaryTechnicalLead: { firstName: { contains: search } } },
+  //           { secondaryTechnicalLead: { lastName: { contains: search } } },
+  //           { name: { contains: search } },
+  //           { description: { contains: search } },
+  //           { licencePlate: { contains: search } }
+  //         ]
+  //       },
+  //       {
+  //         ministry: {
+  //           in: ministry
+  //         }
+  //       },
+  //       {
+  //         cluster: {
+  //           in: cluster
+  //         }
+  //       }
+  //     ]
+  //   },
+  //   skip: offset,
+  //   take: pageSize
+  // });
+  // console.log('project', projects)
   const total = await prisma.privateCloudProject.count({
     where: {
       status: "ACTIVE",
